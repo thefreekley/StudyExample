e6d272107b9677653b30d2fe76e3e7a8fb43e85f
Andrey Matviichuk <thefreekley@gmail.com>
2019-11-29 10:47:43 +0200

    some changes

--- L5Other.hpp
+++ L5Other.hpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <math.h>
 const int n = 5;
-const int m = 5;
 using namespace std;
 
 class Array // клас якій тримає в собі одновимірний масив і дружні функції
 {
 private:
- int dArr[m];
+ int ArrBlock[n];
 public:
  friend void Input_(Array arr[]);
  friend void ShowYourMatrix(Array arr[]);
@@ -18,19 +17,19 @@ public:
 void Input_(Array arr[]) // функція бере в себе масив із елементів класу Array
 {
  for (int i = 0; i < n; i++)
- for (int j = 0; j < m; j++)
+ for (int j = 0; j < n; j++)
  {
  cout << "[" << i + 1 << "][" << j + 1 << "] = ";
- cin >> arr[i].dArr[j];
+ cin >> arr[i].ArrBlock[j];
  }
 }
 void ShowYourMatrix(Array Arr[]) // показ матриці
 {
  for (int i = 0; i < n; i++)
  {
- for (int j = 0; j < m; j++)
+ for (int j = 0; j < n; j++)
  {
- cout << Arr[i].dArr[j] << "\t";
+ cout << Arr[i].ArrBlock[j] << "\t";
  }
  cout << endl;
  }
@@ -43,13 +42,13 @@ void Count(Array arr[]) // розрахунок зазначених математичних дій
  for(int i=0;i<n-1;i++){
  int summ_of=0;
  for (int j = i+1; j <n ; j++) {
- summ_of+=arr[j].dArr[i];
+ summ_of+=arr[j].ArrBlock[i];
  }
  product*=summ_of;
- cout<< "f("<< i <<")=" << summ_of << endl;
+ cout<< "The sum of the elements in each column "<< i <<" under the main diagonal of the matrix -" << summ_of << endl;
  }
  for(int i=0;i<2;i++){
  product=sqrt(abs(product));
  }
- cout << "\n" << "F = " << product << "\n";
+ cout << "\n" << "Geometric mean = " << product << "\n";
 }
